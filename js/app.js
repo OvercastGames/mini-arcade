ee98ce9f352f10c387103ed102e51ff1f7ba734b
Ben Mills <fuzzyllama@gmail.com>
2021-10-04 18:13:47 -0700

    adds starter code for snake

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 'use strict';
 
+function Game(canvas, context) {
+  this.canvas = canvas;
+  this.context = context;
+}
+
+function GameObject(context, x, y, vx, vy) {
+  this.context = context;
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+}
+
+
--- js/snake.js
+++ js/snake.js
@@ -1,4 +1,25 @@
 let canvas = document.getElementById('snake');
+let ctx = canvas.getContext('2d');
 
-canvas.width = 600;
-canvas.height = 450;
+let snakeGame = new Game(canvas, ctx);
+
+snakeGame.canvas.width = 600;
+snakeGame.canvas.height = 450;
+
+function SnakeBody(context, x, y, vx, vy, height, width, color) {
+  GameObject.call(this, context, x, y, vx, vy);
+  this.height = height;
+  this.width = width;
+  this.color = color;
+  this.segments = 5;
+}
+
+SnakeBody.prototype.draw = function () {
+  for (let i = 0; i < this.segments; i++) {
+
+  }
+};
+
+let mySnake = new SnakeBody(ctx, 50, 50, 0, 0, 20, 20, 'green');
+
+
